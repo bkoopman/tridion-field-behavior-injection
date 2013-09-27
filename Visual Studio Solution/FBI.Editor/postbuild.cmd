32b8755db338cdd5a40bc51652656b149c73d6e5
bkoopman@sdl.com <bkoopman@sdl.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-09-27 14:25:09 +0000

    added postbuild.cmd

---
+++ Visual Studio Solution/FBI.Editor/postbuild.cmd
@@ -0,0 +1,5 @@
+﻿REM this files copies the resx file(s) and dll(s) to the Tridion dirs
+REM if you need to modify this file with different paths, don't check that back in
+
+copy /Y "%1Resources\*.resx" "%TRIDION_HOME%web\WebUI\WebRoot\App_GlobalResources"
+copy /Y "%2" "%TRIDION_HOME%web\WebUI\WebRoot\bin\"
