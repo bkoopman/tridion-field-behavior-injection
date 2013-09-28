aee735aac96a510ffa7ce686508b8b7d3f2a6f93
johnwinter@gmail.com <johnwinter@gmail.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-09-28 10:55:24 +0000

    added tab fields

---
+++ trunk/Visual Studio Solution/FBI.Editor/Extensions/ComponentTab/ComponentTab.ascx.cs
@@ -0,0 +1,11 @@
+﻿﻿using Tridion.Web.UI.Controls;
+using Tridion.Web.UI.Core.Controls;
+
+namespace Tridion.Extensions.UI.FBI.Editor.Extensions.ComponentTab
+{
+    [ControlResources("Tridion.Extensions.UI.FBI.ComponentTab")]
+    [ControlResourcesDependency(typeof(TabControl))]
+    public class ComponentTab : Tridion.Web.UI.Controls.TridionUserControl
+    {
+    }
+}
