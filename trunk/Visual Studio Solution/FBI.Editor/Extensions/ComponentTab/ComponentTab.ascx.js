ae761a0ed34c045b2c2256b467cc7db86bc4141f
jsantosalcon@gmail.com <jsantosalcon@gmail.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-10-10 15:09:30 +0000

    I _actually_ did the refactoring (ComponentTab -- FBITab)

--- trunk/Visual Studio Solution/FBI.Editor/Extensions/ComponentTab/ComponentTab.ascx.js
+++ trunk/Visual Studio Solution/FBI.Editor/Extensions/ComponentTab/ComponentTab.ascx.js
@@ -1,24 +1,24 @@
 ﻿Type.registerNamespace("Tridion.Extensions.UI.FBI.Tabs");
-Tridion.Extensions.UI.FBI.Tabs.ComponentTab = function ComponentTab(element) {
-    Tridion.OO.enableInterface(this, "Tridion.Extensions.UI.FBI.Tabs.ComponentTab");
+Tridion.Extensions.UI.FBI.Tabs.FBITab = function ComponentTab(element) {
+    Tridion.OO.enableInterface(this, "Tridion.Extensions.UI.FBI.Tabs.FBITab");
     this.addInterface("Tridion.Controls.DeckPage", [element]);
 };
 
-Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.initialize = function initialize() {
+Tridion.Extensions.UI.FBI.Tabs.FBITab.prototype.initialize = function initialize() {
     this.callBase("Tridion.Controls.DeckPage", "initialize");
     $evt.addEventHandler($display.getItem(), "load", this.getDelegate(this.updateView));
 };
 
-Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.select = function select() {
+Tridion.Extensions.UI.FBI.Tabs.FBITab.prototype.select = function select() {
     this.callBase("Tridion.Controls.DeckPage", "select");
     this.updateView();
 };
 
-Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.updateView = function updateView() {
+Tridion.Extensions.UI.FBI.Tabs.FBITab.prototype.updateView = function updateView() {
     if (this.isSelected()) {
         alert('hello');
 
     }
 };
 
-Tridion.Controls.Deck.registerPageType(Tridion.Extensions.UI.FBI.Tabs.ComponentTab, "FBIComponentTab");
+Tridion.Controls.Deck.registerPageType(Tridion.Extensions.UI.FBI.Tabs.FBITab, "FBITab");
