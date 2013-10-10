32de09e191e8b75b57a0ae316cd19372727350d6
jsantosalcon@gmail.com <jsantosalcon@gmail.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-10-10 15:07:57 +0000

    When registering the Tab, you cannot use "ComponentTab". Seems it might be already in use. So I rename it to FBITab

--- trunk/Visual Studio Solution/FBI.Editor/Extensions/ComponentTab/ComponentTab.ascx.js
+++ trunk/Visual Studio Solution/FBI.Editor/Extensions/ComponentTab/ComponentTab.ascx.js
@@ -1,34 +1,24 @@
 ﻿Type.registerNamespace("Tridion.Extensions.UI.FBI.Tabs");
-
-console.debug("FBI Component Tab JS LOADED");
-
 Tridion.Extensions.UI.FBI.Tabs.ComponentTab = function ComponentTab(element) {
     Tridion.OO.enableInterface(this, "Tridion.Extensions.UI.FBI.Tabs.ComponentTab");
     this.addInterface("Tridion.Controls.DeckPage", [element]);
-    //$j("#FBIComponentTab_switch").hide();
-    console.debug("FBI Component Tab ");
 };
 
 Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.initialize = function initialize() {
-    console.debug("FBI Component Tab Initialize entered");
     this.callBase("Tridion.Controls.DeckPage", "initialize");
     $evt.addEventHandler($display.getItem(), "load", this.getDelegate(this.updateView));
 };
 
 Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.select = function select() {
-    console.debug("FBI Component Tab Selected");
     this.callBase("Tridion.Controls.DeckPage", "select");
     this.updateView();
 };
 
 Tridion.Extensions.UI.FBI.Tabs.ComponentTab.prototype.updateView = function updateView() {
-
-    console.debug("FBI Component Tab Update View Entered");
     if (this.isSelected()) {
-        var html = "hello world";
-        alert(html);
-        $dom.setOuterHTML($("#ComponentTab"), html);
+        alert('hello');
+
     }
 };
 
-Tridion.Controls.Deck.registerPageType(Tridion.Extensions.UI.FBI.Tabs, "ComponentTab");
+Tridion.Controls.Deck.registerPageType(Tridion.Extensions.UI.FBI.Tabs.ComponentTab, "FBIComponentTab");
