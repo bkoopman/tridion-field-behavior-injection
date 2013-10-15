a3043b47a36638aa16a1a8387cd882776e410aae
jsantosalcon@gmail.com <jsantosalcon@gmail.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-10-15 19:48:46 +0000

    Removed Validators

--- Visual Studio Solution/FBI.Model/Services/FBI/FBIServices.svc.cs
+++ Visual Studio Solution/FBI.Model/Services/FBI/FBIServices.svc.cs
@@ -1,38 +1,7 @@
 ﻿using System;
-using System.ServiceModel;
 using System.ServiceModel.Activation;
-using System.ServiceModel.Web;
-using System.Threading;
-
-using System.Collections.Generic;
-using System.Collections;
-
-
-
-using System.Reflection;
-using System.IO;
-using System.Web;
-
-using System.Runtime.Serialization;
-using System.Xml;
-using System.Text;
-using System.Web.Script.Serialization;
-
-
-
-using System.Security;
-
-
-using System.Xml.Linq;
-using System.Linq;
-using Tridion.Extensions.FBI.Model;
 using Tridion.Extensions.Services.Base;
-using Tridion.Extensions.UI.Validators;
 using Tridion.Extensions.FBI.Services.Interfaces;
-
-
-
-
 
 namespace Tridion.Extensions.Services.FBI
 {
