1a2a9a29f68cf5a525b44b4fdad096e17031a97f
bkoopman@sdl.com <bkoopman@sdl.com@87dd918b-b506-4da8-59db-a8f0b7c105cf>
2013-10-22 12:33:33 +0000

    deleted unused FBI.Validators project

--- Visual Studio Solution/FBI.Validators/postbuild.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM this files copies the resx file(s) and dll(s) to the Tridion dirs
-REM if you need to modify this file with different paths, don't check that back in
-
-set TRIDION_PATH=%TRIDION_HOME%
-
-REM copy /Y "%~1Resources\*.resx" "%TRIDION_PATH%web\WebUI\WebRoot\App_GlobalResources"
-copy /Y "%~1%~2" "%TRIDION_PATH%web\WebUI\WebRoot\bin\"
